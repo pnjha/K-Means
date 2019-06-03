9ced3f4fa86c35966348ad20f75d5cadc655d919
Prakash Nath Jha <prakash.nath@students.iiit.ac.in>
2019-06-03 10:12:54 +0530

    added headers

---
+++ header_files.h
@@ -0,0 +1,36 @@
+#ifndef HEADER_H
+#define HEADER_H
+#define euc 11
+#define man 22
+#define che 33
+#define hel 44
+
+#include <bits/stdc++.h> 
+#include <algorithm>
+#include <boost/algorithm/string.hpp>
+#include <omp.h>
+#include <unistd.h>
+#include "create_directories.h"
+#include "plotting.h"
+#include "distance_measure.h"
+#include "metrics.h"
+#include "file_handling.h"
+#include "image_handling.h"
+
+#include <stdio.h> 
+#include <stdlib.h> 
+#include<time.h> 
+#include <fstream>
+
+
+#include "knn.h"
+#include "means.h"
+#include "kmeans.h"
+#include "ensemble.h"
+using namespace std;
+#endif
+
+
+
+
+
